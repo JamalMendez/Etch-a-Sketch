52c37243ed6c391691b430514625114e85bd2538
jamal <jamalmendezfeliz@hotmail.com>
2024-05-03 03:17:50 -0400

    Refactor of the code that change the grid color
    Deleted the complete code that select all divs with a grid class
    name and move the event listener directly into the squareDraw
    function because when the user create a new grid this one doesn't
    change its background-color.

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const squareDraw = (width, height) => {
     for (let i = 0; i < width*height; i++) {
         const div = document.createElement("div");
         div.classList.add("grid");
+
         div.style.width = `calc(100%/${width})`;
         div.style.height = `calc(100%/${height})`;
+        
+        div.addEventListener("mouseover", e => {
+            div.style["background-color"] = "red";
+        });
+
         content.appendChild(div);
     }
 };
 
 squareDraw(16,16);
-
-const div = document.querySelectorAll(".grid");
-
-div.forEach(element => {
-    element.addEventListener("mouseover", e => {
-        element.style["background-color"] = "red";
-    });
-});
 
 const button = document.querySelector("button");
 
