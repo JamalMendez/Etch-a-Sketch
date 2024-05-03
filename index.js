134b676e22dc8cac1d390bc06e742c1779e0479d
jamal <jamalmendezfeliz@hotmail.com>
2024-05-03 03:31:03 -0400

    Grid with random colors
    In the function squareDraw, the div's event listener were added to
    it three variables that contains a random number between 0 and 255
    and with that everytime the user hover a grid it will chages his
    color randomly.

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const content = document.querySelector(".content");
 
 const squareDraw = (width, height) => {
-    for (let i = 0; i < width*height; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid");
+  for (let i = 0; i < width * height; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
 
-        div.style.width = `calc(100%/${width})`;
-        div.style.height = `calc(100%/${height})`;
-        
-        div.addEventListener("mouseover", e => {
-            div.style["background-color"] = "red";
-        });
+    div.style.width = `calc(100%/${width})`;
+    div.style.height = `calc(100%/${height})`;
 
-        content.appendChild(div);
-    }
+    div.addEventListener("mouseover", (e) => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random()) * 256;
+
+      div.style["background-color"] = `rgb(${red} ${green} ${blue})`;
+    });
+
+    content.appendChild(div);
+  }
 };
 
-squareDraw(16,16);
+squareDraw(16, 16);
 
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    const width = prompt("Insert the width you want.");
-    const height = prompt("Insert the height you want.");
+  const width = prompt("Insert the width you want.");
+  const height = prompt("Insert the height you want.");
 
-    const deleteDivs = document.querySelectorAll(".grid");
-    deleteDivs.forEach(grid => {
-        grid.remove();
-    });
+  const deleteDivs = document.querySelectorAll(".grid");
+  deleteDivs.forEach((grid) => {
+    grid.remove();
+  });
 
-    squareDraw(width, height);
+  squareDraw(width, height);
 });
